1a4cd059d02e23b44e17b0fa049af4e873d4df03
alex <alex@izoox.com>
2019-05-09 10:15:41 -0600

    Scraper works but kills the computer

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,27 +5,37 @@ class MainController < ApplicationController
     
     
     def scraper
-        country = Country.all
-        url = Url.all
-        country.each do |country|
-            url.each do |url|
-                source = "https://tradingeconomics.com/#{country.name}/#{url.slug}"
-                unparsed_page = HTTParty.get(source)
-                parsed_page = Nokogiri::HTML(unparsed_page)
-                @table_headers = parsed_page.css('#ctl00_ContentPlaceHolder1_ctl03_titleHeader').text
-                indicators = parsed_page.css("#ctl00_ContentPlaceHolder1_ctl03_PanelDefinition")
-                indicators.each do |indicator|
-                    @indicator = {
-                        actual: indicator.css("td:nth-child(2)").text,
-                        previous: indicator.css("td:nth-child(3)").text,
-                        highest: indicator.css("td:nth-child(4)").text,
-                        lowest: indicator.css("td:nth-child(5)").text,
-                        dates: indicator.css("td:nth-child(6)").text,
-                        unit: indicator.css("td:nth-child(7)").text,
-                        frequency: indicator.css("td:nth-child(8)").text
-                    }
-                end
-            end
+        countries = Country.all
+        urls = Url.all
+        url_list = []
+
+        countries.each do |country|
+            urls.each do |url|
+               url_list << "https://tradingeconomics.com/#{country.name}/#{url.slug}"
+            end 
+        end
+
+        unparsed_pages = url_list.each do |u| 
+            HTTParty.get(u)
+            puts "successfully requested URI"
+        end
+        parsed_pages = unparsed_pages.each do |up|
+            Nokogiri::HTML(up)
+            puts "successfully parsed pages"
+        end
+
+        @table_headers = parsed_pages.css('#ctl00_ContentPlaceHolder1_ctl03_titleHeader').text
+        indicators = parsed_pages.css("#ctl00_ContentPlaceHolder1_ctl03_PanelDefinition")
+        indicators.each do |indicator|
+            @indicator = {
+                actual: indicator.css("td:nth-child(2)").text,
+                previous: indicator.css("td:nth-child(3)").text,
+                highest: indicator.css("td:nth-child(4)").text,
+                lowest: indicator.css("td:nth-child(5)").text,
+                dates: indicator.css("td:nth-child(6)").text,
+                unit: indicator.css("td:nth-child(7)").text,
+                frequency: indicator.css("td:nth-child(8)").text
+            }
         end
     end
     
