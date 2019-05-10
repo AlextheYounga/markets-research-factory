d55caa23ebb71d2b9de5d45b341d477ab12d67c5
alex <alex@izoox.com>
2019-05-09 20:06:32 -0600

    Successful scrape, going to brake the scraper into chunks so it's not breaking everything

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,12 +2,14 @@ class MainController < ApplicationController
     require 'httparty'
     require 'nokogiri'
     require 'byebug'
-    
-    
+
+
     def scraper
         countries = Country.all
         urls = Url.all
         url_list = []
+        unparsed_pages = []
+        @table_headers = []
 
         countries.each do |country|
             urls.each do |url|
@@ -15,30 +17,28 @@ class MainController < ApplicationController
             end 
         end
 
-        unparsed_pages = url_list.each do |u| 
-            HTTParty.get(u)
-            puts "successfully requested URI"
-        end
-        parsed_pages = unparsed_pages.each do |unparsed_page|
-            Nokogiri::HTML(unparsed_page)
-            puts "successfully parsed pages"
+        url_list.each do |u| 
+            unparsed_pages << Nokogiri::HTML(HTTParty.get(u))
+            puts 'page scanned and added to array'
         end
 
-        @table_headers = parsed_pages.each.css('#ctl00_ContentPlaceHolder1_ctl03_titleHeader').text
-
-
-        indicators = parsed_pages.css("#ctl00_ContentPlaceHolder1_ctl03_PanelDefinition")
-        indicators.each do |indicator|
-            @indicator = {
-                actual: indicator.css("td:nth-child(2)").text,
-                previous: indicator.css("td:nth-child(3)").text,
-                highest: indicator.css("td:nth-child(4)").text,
-                lowest: indicator.css("td:nth-child(5)").text,
-                dates: indicator.css("td:nth-child(6)").text,
-                unit: indicator.css("td:nth-child(7)").text,
-                frequency: indicator.css("td:nth-child(8)").text
-            }
+        unparsed_pages.each do |unparsed_page|
+            @table_headers << unparsed_page.css('#ctl00_ContentPlaceHolder1_ctl03_titleHeader').text.strip
+            puts "header grabbed"
         end
+
+        # indicators = parsed_pages.css("#ctl00_ContentPlaceHolder1_ctl03_PanelDefinition")
+        # indicators.each do |indicator|
+        #     @indicator = {
+        #         actual: indicator.css("td:nth-child(2)").text,
+        #         previous: indicator.css("td:nth-child(3)").text,
+        #         highest: indicator.css("td:nth-child(4)").text,
+        #         lowest: indicator.css("td:nth-child(5)").text,
+        #         dates: indicator.css("td:nth-child(6)").text,
+        #         unit: indicator.css("td:nth-child(7)").text,
+        #         frequency: indicator.css("td:nth-child(8)").text
+        #     }
+        # end
 
     end
     
