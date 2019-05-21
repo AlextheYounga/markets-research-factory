9ea75bb731c38fd9d795a15f56d53636546f240e
alex <alex@izoox.com>
2019-05-20 19:55:53 -0600

    Excel export works

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,7 +26,7 @@ function generateExcel() {
 
     var a = document.createElement('a');
     a.href = data_type + ', ' + table_html;
-    a.download = 'Example_Table_To_Excel.xls';
+    a.download = 'TradingEconomicsData.xls';
     a.click();
 }
 
